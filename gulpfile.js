6e2405706700b3be2a6a35d6364b147d8237f4d5
Corey Bruyere <coreybruyere@ymail.com>
2017-02-02 20:35:21 -0800

    Add config.json to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 /** 
- * Require plugins
+ * Requirements
  */
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var gulpShopify = require('gulp-shopify-upload');
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const gulpShopify = require('gulp-shopify-upload');
+const config = require('./config.json');
 
 /** 
  * Watch Task
  */
 gulp.task('shopifywatch', function() {
   return watch('./+(assets|layout|config|snippets|templates|locales)/**')
-.pipe(gulpShopify('f36cd30ebf56814f1836d2acb9cceb6b', 'd891359435b5ce1b8bb350f781d9850d', 'shay-designs-jewelry.myshopify.com', '166966279'));
+.pipe(gulpShopify(config.shopify_api_key, config.shopify_api_password, config.shopify_url, config.shopify_theme_id));
 });
 
 /** 
